c81a5ea9d0e440533132f7742d511935c4153948
givensuman <givensuman@duck.com>
2024-09-21 16:33:08 -0400

    Add even more test

--- rdyset_test.go
+++ rdyset_test.go
@@ -179,3 +179,119 @@ func TestEquals(t *testing.T) {
 		fail(t, expect)
 	}
 }
+
+func TestUnion(t *testing.T) {
+	var expect bool
+	s := Set(1, 2, 3)
+
+	expect = s.Union(Set(4)).Equals(Set(1, 2, 3, 4))
+	if !expect {
+		fail(t, expect)
+	}
+
+	expect = s.Union(Set(3, 4)).Equals(Set(1, 2, 3, 4))
+	if !expect {
+		fail(t, expect)
+	}
+
+	expect = s.Union(Set[int]()).Equals(s)
+	if !expect {
+		fail(t, expect)
+	}
+}
+
+func TestIntersection(t *testing.T) {
+	var expect bool
+	s := Set(1, 2, 3)
+
+	expect = s.Intersection(Set(3, 4, 5)).Equals(Set(3))
+	if !expect {
+		fail(t, expect)
+	}
+
+	expect = s.Intersection(Set[int]()).Equals(Set[int]())
+	if !expect {
+		fail(t, expect)
+	}
+}
+
+func TestDifference(t *testing.T) {
+	var expect bool
+	s := Set(1, 2, 3)
+
+	expect = s.Difference(Set(3)).Equals(Set(1, 2))
+	if !expect {
+		fail(t, expect)
+	}
+
+	expect = s.Difference(Set(4, 5, 6)).Equals(s)
+	if !expect {
+		fail(t, expect)
+	}
+
+	expect = s.Difference(Set(1, 2, 3, 4, 5, 6)).Equals(Set[int]())
+	if !expect {
+		fail(t, expect)
+	}
+
+	expect = s.Difference(Set[int]()).Equals(s)
+	if !expect {
+		fail(t, expect)
+	}
+}
+
+func TestSymmetricDifference(t *testing.T) {
+	var expect bool
+	s := Set(1, 2, 3)
+
+	expect = s.SymmetricDifference(Set(4, 5, 6)).Equals(Set(1, 2, 3, 4, 5, 6))
+	if !expect {
+		fail(t, expect)
+	}
+
+	expect = s.SymmetricDifference(Set(3, 4, 5)).Equals(Set(1, 2, 4, 5))
+	if !expect {
+		fail(t, expect)
+	}
+
+	expect = s.SymmetricDifference(Set[int]()).Equals(s)
+	if !expect {
+		fail(t, expect)
+	}
+}
+
+func TestForEach(t *testing.T) {
+	// TODO
+}
+
+func TestFilter(t *testing.T) {
+	var expect bool
+	s := Set(1, 2, 3)
+
+	expect = s.Filter(func(k int) bool {
+		return k > 1
+	}).Equals(Set(2, 3))
+	if !expect {
+		fail(t, expect)
+	}
+}
+
+func TestMap(t *testing.T) {
+	var expect bool
+	s := Set(1, 2, 3)
+
+	expect = s.Map(func(k int) int {
+		return k * k
+	}).Equals(Set(1, 4, 9))
+	if !expect {
+		fail(t, expect)
+	}
+}
+
+func TestArray(t *testing.T) {
+	// TODO
+}
+
+func TestString(t *testing.T) {
+	// TODO
+}
